025390c21c2326b820c50f9d84307d652f242e3a
Prateek Patil <prateekpatil096@gmail.com>
2026-03-14 09:31:39 +0530

    output methods

---
+++ lec2/variables.java
@@ -0,0 +1,8 @@
+class variables{
+    public static void main(String[] args) {
+        System.out.print("hello world in java\n");
+        System.out.println("hello world in java");
+        System.out.print("hello world in java \n from prateek");
+    }
+    
+}
