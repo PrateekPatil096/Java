8ed45b594ee9bff2f0f538bce6490875e92a4269
Prateek Patil <prateekpatil096@gmail.com>
2026-03-14 17:54:01 +0530

    variabless

--- lec2/variables.java
+++ lec2/variables.java
@@ -3,6 +3,18 @@ class variables{
         System.out.print("hello world in java\n");
         System.out.println("hello world in java");
         System.out.print("hello world in java \n from prateek");
+
+        int a=20;
+        int b=60;
+        int sum=a+b;
+        System.out.println(sum);
+
+        int mul=a*b;
+        System.out.println(mul);
+        
+        int diff=a-b;
+        System.out.println(diff);
+
     }
     
 }
