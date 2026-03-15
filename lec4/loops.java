cb026cad741bd1b4da403300c3c41ab310ab04ab
Prateek Patil <prateekpatil096@gmail.com>
2026-03-15 10:41:12 +0530

    for loop

---
+++ lec4/loops.java
@@ -0,0 +1,12 @@
+public class loops {
+    public static void main(String args[]){
+        for(int i=0;i<11;i=i+1){
+            System.out.println("toji");
+        }
+        for(int j=0;j<11;j++){
+            System.out.println(j+" ");
+
+        }
+    }
+    
+}
