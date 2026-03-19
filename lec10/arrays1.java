0b7dad470bbd54eb1459b355819585ca13c482c7
Prateek Patil <prateekpatil096@gmail.com>
2026-03-19 09:15:56 +0530

    arrays basics

---
+++ lec10/arrays1.java
@@ -0,0 +1,18 @@
+
+public class arrays1 {
+    public static void main(String[] args) {
+        int [] marks=new int[3];
+        marks[0]=90;
+        marks[1]=98;
+        marks[2]=96;
+        System.out.println(marks[0]);
+         System.out.println(marks[1]);
+          System.out.println(marks[2]);
+
+          for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+          }
+
+    }
+    
+}
