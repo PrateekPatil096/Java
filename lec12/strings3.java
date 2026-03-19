321d5f0519faeae4bf12fa23a14150d53adf4429
Prateek Patil <prateekpatil096@gmail.com>
2026-03-19 21:36:11 +0530

    strings functions

---
+++ lec12/strings3.java
@@ -0,0 +1,12 @@
+public class strings3 {
+    public static void main(String[] args) {
+        String name1="toji";
+        String name2="zenin";
+        if(name1.compareTo(name2)==0){
+            System.out.println("strings are equal");
+        }else{
+            System.out.println("strings are not equal");
+        }
+    }
+    
+}
