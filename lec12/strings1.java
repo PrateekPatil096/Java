a6084058ac55709407bf7f1ff3893ec7bd39cafe
Prateek Patil <prateekpatil096@gmail.com>
2026-03-19 21:27:23 +0530

    strings in java

---
+++ lec12/strings1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class strings1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("your name is "+ name);
+
+
+    }
+    
+}
