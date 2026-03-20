e14df1cfa49068bf08be628d39f73910a9fb3257
Prateek Patil <prateekpatil096@gmail.com>
2026-03-20 09:04:57 +0530

    string buliders

---
+++ lec13/stringb.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class stringb {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("toji");
+        System.out.println(sb);
+    }
+
+}
