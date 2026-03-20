9917b4c9c76b33cbec7735da761d4f8a0981dd6f
Prateek Patil <prateekpatil096@gmail.com>
2026-03-20 09:07:52 +0530

    strings functions

--- lec13/stringb.java
+++ lec13/stringb.java
@@ -5,6 +5,15 @@ public class stringb {
 
         StringBuilder sb = new StringBuilder("toji");
         System.out.println(sb);
+        System.out.println(sb.charAt(0));
+        sb.setCharAt(0,'o');
+        System.out.println(sb);
+
+        sb.insert(0,'s');
+        System.out.println(sb);
+
+        sb.delete(2,3);
+        System.out.println(sb);
     }
 
 }
