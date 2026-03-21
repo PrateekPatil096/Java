c8d0dd59a9c28767c18773486a6558236d1cac6c
Prateek Patil <prateekpatil096@gmail.com>
2026-03-21 21:39:52 +0530

    recursion class basics

---
+++ lec17/recursion1_0.java
@@ -0,0 +1,14 @@
+public class recursion1_0 {
+    public static void printnumber(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        printnumber(n-1);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        printnumber(n);
+    }
+    
+}
