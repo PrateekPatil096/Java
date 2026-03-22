d30e9c2b5c12b7df8e84af1e1217d75937c2293d
Prateek Patil <prateekpatil096@gmail.com>
2026-03-22 09:27:51 +0530

    printing i

--- lec17/recursion1_2.java
+++ lec17/recursion1_2.java
@@ -7,6 +7,7 @@ public class recursion1_2 {
         }
         sum=sum+i;
         printSum(i+1, n, sum);
+        System.out.println(i);
     }
     public static void main(String[] args) {
         printSum(1,5,0);
