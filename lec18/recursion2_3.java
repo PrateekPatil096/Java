8699d1082796e5f4c6d2bc97651b075215125410
Prateek Patil <prateekpatil096@gmail.com>
2026-03-23 22:36:22 +0530

    finding first and last occurance of element in string

---
+++ lec18/recursion2_3.java
@@ -0,0 +1,26 @@
+public class recursion2_3 {
+    public static int first=-1;
+    public static int last=-1;
+    
+    public static void findOccurnce(String str,int idx,char element){
+        if(idx==str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char currChar=str.charAt(idx);
+        if(currChar==element){
+            if(first==-1){
+                first=idx;
+            }
+            else{
+                last=idx;
+            }
+        }
+        findOccurnce(str, idx+1, element);
+    }
+    public static void main(String[] args) {
+        String str="abaacdaefaah";
+        findOccurnce(str, 0, 'a');
+    }
+}
