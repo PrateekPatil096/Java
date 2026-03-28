0266a0b17df623e19a68954e81e3cd8803d654e1
Prateek Patil <prateekpatil096@gmail.com>
2026-03-28 22:05:41 +0530

    permutation of string

---
+++ lec19/recadv.java
@@ -0,0 +1,17 @@
+public class recadv {
+    public static void printPrem(String str,String permutation){
+        if(str.length()==0){
+            System.out.println(permutation);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char curchar=str.charAt(i);
+            String newStr=str.substring(0,i)+str.substring(i+1);
+            printPrem(newStr, permutation+curchar);
+        }
+    }
+    public static void main(String[] args) {
+        String str ="abc";
+        printPrem(str, " ");
+    }
+}
