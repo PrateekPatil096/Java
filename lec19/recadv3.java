8f67b7b8e42d3e72214a0c1dea038b1c0a4e187c
Prateek Patil <prateekpatil096@gmail.com>
2026-03-29 09:35:34 +0530

    place  tiles of size in floor

---
+++ lec19/recadv3.java
@@ -0,0 +1,18 @@
+public class recadv3 {
+    public static int placeTiles(int n, int m ){
+        if(n==m){
+            return 2;
+        }
+        if(n<m){
+            return 1;
+        }
+        int vertplacements=placeTiles(n-m, m);
+        int horplacements=placeTiles(n-1, m);
+        return vertplacements+horplacements;
+    }
+    public static void main(String[] args) {
+        int n=4 ,m=2;
+        System.out.println(placeTiles(n, m));
+    }
+    
+}
