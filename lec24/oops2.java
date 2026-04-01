015d950a6ba96d5a88f44e26fa4c26240b3dd720
Prateek Patil <prateekpatil096@gmail.com>
2026-04-01 14:50:32 +0530

    copy constructor

--- lec24/oops2.java
+++ lec24/oops2.java
@@ -5,14 +5,20 @@ class Stundent{
         System.out.println(this.name);
         System.out.println(this.age);
     }
-    Stundent(String name,int age){
-        this.name=name;
-        this.age=age;
+    Stundent(Stundent s2){
+        this.name=s2.name;
+        this.age=s2.age;
+    }
+    Stundent(){
+
     }
 }
 public class oops2 {
     public static void main(String[] args) {
-        Stundent s1=new Stundent("toji", 22);
-        s1.printInfo();
+        Stundent s1=new Stundent();
+        s1.name="toji";
+        s1.age=22;
+        Stundent s2=new Stundent(s1);
+        s2.printInfo();
     }
 }
