97982971e5d2728cdac9f2201ec1c50cf2e8fe5c
Prateek Patil <prateekpatil096@gmail.com>
2026-04-02 07:44:59 +0530

    abstraction in oops

---
+++ lec24/oops4.java
@@ -0,0 +1,21 @@
+abstract class Animal{
+    abstract void walk();
+
+}
+class Horse extends Animal{
+    public void walk(){
+        System.out.println("walk on 4 legs");
+    }
+}
+class Chicken extends Animal{
+    public void walk(){
+        System.out.println("walk on 2 legs");
+    }
+}
+public class oops4 {
+    public static void main(String[] args) {
+        Horse horse=new Horse();
+        horse.walk();
+    }
+    
+}
