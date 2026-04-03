5e12d75d3ba2caedfff469f70bbca77e4f8e31f8
Prateek Patil <prateekpatil096@gmail.com>
2026-04-03 09:22:19 +0530

    arraylist in java

---
+++ lec25/arraylists.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class arraylists {
+    public static void main(String[] args) {
+        ArrayList<Integer>list=new ArrayList<Integer>();
+
+        list.add(0);
+        list.add(1);
+        list.add(3);
+        System.out.println(list);
+    }
+    
+}
