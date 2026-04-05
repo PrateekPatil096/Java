92a67e9f664fa5bf96951610adf73276e5c6b1f8
Prateek Patil <prateekpatil096@gmail.com>
2026-04-05 10:43:34 +0530

    linked list in java

--- lec27/LL.java
+++ lec27/LL.java
@@ -1,46 +1,63 @@
-import org.w3c.dom.Node;
-
 class LL {
+
     Node head;
-    class Node{
+
+    class Node {
         String data;
         Node next;
 
-        Node(String data){
-            this.data=data;
-            this.next=null;
+        Node(String data) {
+            this.data = data;
+            this.next = null;
         }
-    }
-    public void addFirst(String data){
-        Node newNode=new Node(data);
-        if(head==null){
-            head=newNode;
-            return;
-        }
-        Node currNode=head;
-        while(currNode.next!=null){
-            currNode=currNode.next;
-        }
-        currNode.next=newNode;
-    }
-    public void printlist(){
-        if(head==null){
-            System.out.println("list is empty");
-            return;
-        }
-        Node currNode=head;
-        while(currNode!=null){
-            System.out.println(currNode.data+"-->");
-            currNode=currNode.next;
-        }
-        System.out.println("null");
-    }
-    public static void main(String[] args) {
-        LL list=new LL();
-        list.addFirst("a");
-        list.addFirst("is");
-        list.printlist();
     }
 
     
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;   
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        
+        currNode.next = newNode;  
+    }
+
+    
+    public void printlist() {
+        if (head == null) {
+            System.out.println("list is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.println(currNode.data + "-->");
+            currNode = currNode.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+
+        list.addFirst("a");      
+        list.addFirst("is");     
+
+        list.printlist();        
+
+        list.addLast("list");    
+
+        list.printlist();        
+    }
 }
