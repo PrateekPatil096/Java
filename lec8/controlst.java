aab3411e720453ee4d41a5106938a616a83cbc2f
Prateek Patil <prateekpatil096@gmail.com>
2026-04-12 22:07:22 +0530

    boolean statement

---
+++ lec8/controlst.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class controlst {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         System.out.println("enter x vlaue= ");
+         int x=sc.nextInt();
+
+boolean result = (x > 5);  // true
+
+if (result) {
+    System.out.println("x is greater than 5");
+}
+    }
+    
+}
