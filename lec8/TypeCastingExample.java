5f75b07e1b025fb92c1ba454547fa599124c6323
Prateek Patil <prateekpatil096@gmail.com>
2026-04-13 21:51:21 +0530

    type casting

---
+++ lec8/TypeCastingExample.java
@@ -0,0 +1,11 @@
+class TypeCastingExample {
+    public static void main(String args[]) {
+        double a = 9.78;
+        int b;
+
+        b = (int) a;   // casting double to int
+
+        System.out.println("Double value: " + a);
+        System.out.println("Int value: " + b);
+    }
+}
