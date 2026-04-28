c17f8d3caa3079966194d534d07f9513a07b7fad
Prateek Patil <prateekpatil096@gmail.com>
2026-04-28 07:58:18 +0530

    queue using linked list

--- lec31/circularq.java
+++ lec31/circularq.java
@@ -1,33 +1,35 @@
 public class circularq{
+     static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            next=null;
+        }
+    }
     static class queue{
-        static int arr[];
-        static int size;
-        static int rear=-1;
-        static int front=-1;
+        static Node head=null;
+        static Node tail=null;
         
-        queue(int n){
-            arr=new int [n];
-            this.size=n;
-        }
+        
+       
         public static boolean isEmpty(){
-            return rear==-1 && front==-1;
+            return head==null & tail==null;
         }
 
-        public static boolean isFull(){
-            return (rear+1)% size== front;
-        }
-
+       
         //enqueu
         public static void add(int data){
-            if(isFull()){
-                System.out.println("full queue");
+            Node newNode=new Node(data);
+            if(tail==null){
+                tail=head=newNode;
                 return;
             }
-            if(front==-1){
-                front=0;
-            }
-            rear=(rear+1)%size;
-            arr[rear]=data;
+            tail.next=newNode;
+            tail=newNode;
+            
+           
         }
         //dequeue
         public static int remove(){
@@ -35,14 +37,13 @@ public class circularq{
                 System.out.println("empty queue");
                 return -1;
             }
-            int result=arr[front];
-            if(rear==front){
-                rear=front=-1;
-            } else{
-                front=(front+1)%size;
-            }
-            
-            return result;
+           int front=head.data;
+           if(head==tail){
+            tail=null;
+           }
+           head=head.next;
+
+           return front;
         }
         //peek
         public static int peek(){
@@ -50,20 +51,17 @@ public class circularq{
                 System.out.println("empty queue");
                 return-1;
             }
-            return arr[front];
+            return head.data;
         }
     }
     public static void main(String[] args) {
-        queue q= new queue(5);
+        queue q= new queue();
         q.add(1);
         q.add(2);
         q.add(3);
         q.add(4);
         q.add(5);
-        System.out.println(q.remove());
-        q.add(6);
-        System.out.println(q.remove());
-        q.add(7);
+       
 
         while(!q.isEmpty()){
             System.out.println(q.peek());
