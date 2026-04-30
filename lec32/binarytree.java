23326d6867d266908d91d42c381c61d20b99965f
Prateek Patil <prateekpatil096@gmail.com>
2026-04-30 08:45:24 +0530

    preorder traversal

--- lec32/binarytree.java
+++ lec32/binarytree.java
@@ -31,6 +31,14 @@ public class binarytree {
             return newNode;
         }
     }
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
 
     public static void main(String args[]) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -38,6 +46,6 @@ public class binarytree {
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
 
-        System.out.println(root.data);
+        preorder(root);
     }
 }
