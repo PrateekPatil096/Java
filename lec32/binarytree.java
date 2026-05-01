27157eb7ab8f7f3a36c856aa6e99e3ee9f798159
Prateek Patil <prateekpatil096@gmail.com>
2026-05-01 20:30:41 +0530

    inorder traversal in tree

--- lec32/binarytree.java
+++ lec32/binarytree.java
@@ -39,6 +39,14 @@ public class binarytree {
         preorder(root.left);
         preorder(root.right);
     }
+    public static void inOrder(Node root){
+        if(root ==null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
 
     public static void main(String args[]) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -46,6 +54,6 @@ public class binarytree {
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
 
-        preorder(root);
+        inOrder(root);
     }
 }
