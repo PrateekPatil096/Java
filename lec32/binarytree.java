935dd02f6a429fd11115ab7766ba4ca1ae911eb6
Prateek Patil <prateekpatil096@gmail.com>
2026-05-02 22:23:45 +0530

    post order traversal

--- lec32/binarytree.java
+++ lec32/binarytree.java
@@ -47,6 +47,14 @@ public class binarytree {
         System.out.print(root.data+" ");
         inOrder(root.right);
     }
+    public static void postOrder(Node root ){
+        if(root == null){
+            return ;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data+" ");
+    }
 
     public static void main(String args[]) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -54,6 +62,6 @@ public class binarytree {
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
 
-        inOrder(root);
+        postOrder(root);
     }
 }
