b7050e52f16ca75dbd4915f59bd6c144ee9e8a98
Prateek Patil <prateekpatil096@gmail.com>
2026-05-03 09:51:33 +0530

    levelorder traversal

--- lec32/binarytree.java
+++ lec32/binarytree.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 public class binarytree {
 
     static class Node {
@@ -55,6 +56,42 @@ public class binarytree {
         postOrder(root.right);
         System.out.print(root.data+" ");
     }
+   public static void levelOrder(Node root) {
+    if (root == null) {
+        return;
+    }
+
+    Queue<Node> q = new LinkedList<>();
+
+    q.add(root);
+    q.add(null);
+
+    while (!q.isEmpty()) {
+        Node currNode = q.remove();
+
+        if (currNode == null) {
+            System.out.println();
+
+            if (q.isEmpty()) {
+                break;
+            } else {
+                q.add(null);
+            }
+        } else {
+            System.out.print(currNode.data + " ");
+
+            if (currNode.left != null) {
+                q.add(currNode.left);
+            }
+
+            if (currNode.right != null) {
+                q.add(currNode.right);
+            }
+        }
+    }
+}
+        
+    
 
     public static void main(String args[]) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -62,6 +99,6 @@ public class binarytree {
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
 
-        postOrder(root);
+        levelOrder(root);
     }
 }
