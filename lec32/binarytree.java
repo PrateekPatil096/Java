b660bb556f3d3c795f67f27b0d5062bdcdad643d
Prateek Patil <prateekpatil096@gmail.com>
2026-05-06 14:38:35 +0530

    diameter of binary tree

--- lec32/binarytree.java
+++ lec32/binarytree.java
@@ -119,6 +119,16 @@ public static int height(Node root){
     int myHeight=Math.max(leftHeight,rightHeight)+1;
     return myHeight;
 }
+public static int diameter(Node root){
+    if(root==null){
+        return 0;
+    }
+    int diam1=diameter(root.left);
+    int diam2=diameter(root.right);
+    int diam3=height(root.left)+height(root.right)+1;
+
+    return Math.max(diam3,Math.max(diam1,diam2));
+}
         
     
 
@@ -128,6 +138,6 @@ public static int height(Node root){
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
 
-        System.out.println(height(root));
+        System.out.println(diameter(root));
     }
 }
