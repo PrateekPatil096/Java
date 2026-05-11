eaca60df3cce0b209fcb9b0784b52c18cfde5f51
Prateek Patil <prateekpatil096@gmail.com>
2026-05-11 18:54:51 +0530

    delete a node in BST

--- lec33/bst.java
+++ lec33/bst.java
@@ -44,6 +44,35 @@ public class bst {
             return Search(root.right, key);
         }
     }
+
+    public static Node delete(Node root,int val){
+        if(root.data>val){
+            root.left=delete(root.left,val);
+        }
+        else{
+            if(root.left==null && root.right==null){
+                return null;
+            }
+            if(root.left==null){
+                return root.right;
+            }
+            else if(root.right==null){
+                return root.left;
+            }
+        Node IS=inorderSuccesser(root.right);
+        root.data=IS.data;
+        root.right=delete(root.right,IS.data);
+        }
+        return root;
+       
+    }
+    public static Node inorderSuccesser(Node root){
+        while(root.left!=null){
+            root=root.left;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
         int values[]={8,5,3,1,4,6,10,11,14};
         Node root=null;
@@ -54,12 +83,8 @@ public class bst {
         inorder(root);
         System.out.println();
 
-        if(Search(root, 1)){
-            System.out.println("found");
-        }
-        else{
-            System.out.println("mot found");
-        }
+        delete(root, 4);
+        inorder(root);
         
     }
     
