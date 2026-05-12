7c5aff798492be983c37a2abfece2b9300e1b796
Prateek Patil <prateekpatil096@gmail.com>
2026-05-12 22:49:24 +0530

    root of leaf paths

--- lec33/bst.java
+++ lec33/bst.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class bst {
     static class Node{
         int data;
@@ -49,6 +51,9 @@ public class bst {
         if(root.data>val){
             root.left=delete(root.left,val);
         }
+        else if(root.data<val){
+            root.right=delete(root.right,val);
+        }
         else{
             if(root.left==null && root.right==null){
                 return null;
@@ -88,6 +93,40 @@ public class bst {
             printinrange(root.right, x, y);
         }
     }
+    public static void printPath(ArrayList<Integer> path) {
+
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + " -> ");
+        }
+
+        System.out.println("null");
+    }
+
+    
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path) {
+
+        if (root == null) {
+            return;
+        }
+
+        
+        path.add(root.data);
+
+        
+        if (root.left == null && root.right == null) {
+            printPath(path);
+        } else {
+
+            
+            printRoot2Leaf(root.left, path);
+
+            
+            printRoot2Leaf(root.right, path);
+        }
+
+        
+        path.remove(path.size() - 1);
+    }
 
     public static void main(String[] args) {
         int values[]={8,5,3,1,4,6,10,11,14};
@@ -99,7 +138,7 @@ public class bst {
         inorder(root);
         System.out.println();
 
-       printinrange(root, 6, 10);
+       printRoot2Leaf(root,new ArrayList<>());
         
     }
     
