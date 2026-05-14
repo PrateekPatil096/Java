126b3c5dff79bd769c53fddd0fa29fa671f6fbc7
Prateek Patil <prateekpatil096@gmail.com>
2026-05-14 18:22:08 +0530

    hashmap insertion

---
+++ lec35/hashmap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.HashMap;
+
+public class hashmap {
+
+    public static void main(String[] args) {
+        HashMap<String,Integer> map=new HashMap<>();
+
+        map.put("india",120);
+        map.put("usa",30);
+        map.put("china",150);
+
+        System.out.println(map);
+    }
+
+    
+}
