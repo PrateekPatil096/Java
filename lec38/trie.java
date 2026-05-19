7c7d5433e2e86268c00598311fb2195880922561
Prateek Patil <prateekpatil096@gmail.com>
2026-05-19 19:06:54 +0530

    defining trie

---
+++ lec38/trie.java
@@ -0,0 +1,20 @@
+
+
+public class trie {
+    static  class Node{
+        Node[] children;
+        boolean eow;
+    }
+    public Node(){
+        children=new Node[26];
+        for(int i=0;i<26;i++){
+            children[i]=null;
+        }
+        eow=false;
+    }
+    static Node root=new Node();
+    public static void main(String[] args) {
+        String words[]={"the","a","there","their","any"};
+    }
+    
+}
