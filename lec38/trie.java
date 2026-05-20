c87bc90a583528610168ad89713fd0d289db82af
Prateek Patil <prateekpatil096@gmail.com>
2026-05-20 15:56:55 +0530

    search in trie

--- lec38/trie.java
+++ lec38/trie.java
@@ -4,32 +4,58 @@ public class trie {
     static  class Node{
         Node[] children;
         boolean eow;
-    }
-    public Node(){
+    
+    public  Node(){
         children=new Node[26];
         for(int i=0;i<26;i++){
             children[i]=null;
         }
         eow=false;
     }
+}
     static Node root=new Node();
     public static void insert(String words){
+        Node curr=root;
         for(int i=0;i<words.length();i++){
             int idx=words.charAt(i)-'a';
-            if(root.children[idx]==null){
-                root.children[idx]=new Node();
+            if(curr.children[idx]==null){
+                curr.children[idx]=new Node();
             }
             if(i==words.length()-1){
-                root.children[idx].eow=true;
+                curr.children[idx].eow=true;
             }
-            root=root.children[idx];
+            curr=curr.children[idx];
         }
     }
+    public static boolean search(String key) {
+        Node curr=root;
+
+    for(int i = 0; i < key.length(); i++) {
+
+        int idx = key.charAt(i) - 'a';
+        Node node = curr.children[idx];
+
+        if(node == null) {
+            return false;
+        }
+
+        if(i == key.length() - 1 && node.eow == false) {
+            return false;
+        }
+
+        curr = curr.children[idx];
+    }
+
+    return true;
+}
     public static void main(String[] args) {
         String words[]={"the","a","there","their","any"};
         for(int i=0;i<words.length;i++){
             insert(words[i]);
         }
+        System.out.println(search("their"));
+        System.out.println(search("thor"));
+        System.out.println(search("an"));
     }
     
 }
