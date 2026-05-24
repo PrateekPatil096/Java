60d9860ea98efa000bd66814e4d5c5961bbc96e0
Prateek Patil <prateekpatil096@gmail.com>
2026-05-24 18:56:43 +0530

    longest word with all prefixes

--- lec38/trie.java
+++ lec38/trie.java
@@ -88,14 +88,38 @@ public static int countNode(Node root){
     }
     return count+1;
 }
-    public static void main(String[] args) {
-        String str="ababa";
+public static String ans="";
+public static void longestWord(Node root, StringBuilder temp) {
 
-        for(int i=0;i<str.length();i++){
-            String suffix =str.substring(i);
-            insert(suffix);
+    if (root == null) {
+        return;
+    }
+
+    for (int i = 0; i < 26; i++) {
+
+        if (root.children[i] != null &&
+            root.children[i].eow == true) {
+
+            temp.append((char)(i + 'a'));
+
+            if (temp.length() > ans.length()) {
+                ans = temp.toString();
+            }
+
+            longestWord(root.children[i], temp);
+
+            temp.deleteCharAt(temp.length() - 1);
         }
-        System.out.println(countNode(root));
+    }
+}
+    public static void main(String[] args) {
+        String words[]={"a","banana","app","appl","ap","apply","apple"};
+
+       for(int i=0;i<words.length;i++){
+        insert(words[i]);
+       }
+       longestWord(root, new StringBuilder(""));
+       System.out.println(ans);
 
     }
     
