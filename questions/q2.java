c1c4ddbda4e13b657e2d0d238d7734c405f3158c
Prateek Patil <prateekpatil096@gmail.com>
2026-06-16 12:03:18 +0530

    swapping of number without temp variable

---
+++ questions/q2.java
@@ -0,0 +1,19 @@
+
+
+public class q2 {
+    public static void main (String args[]){
+        int a=10;
+        int b=20;
+        System.out.println("before swapping ");
+        System.out.println(a);
+        System.out.println(b);
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("after swapping");
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
