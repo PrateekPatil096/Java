71c865de4949f9cea7a0621ffd1d1318edb9b2e3
Prateek Patil <prateekpatil096@gmail.com>
2026-06-18 10:48:26 +0530

    factorial of number

---
+++ questions/q8.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class q8 {
+    public static void main(String [] args){
+        Scanner sc =new Scanner(System.in);
+         
+        System.out.println("enetr the number ");
+        int n=sc.nextInt();
+        int f=1;
+        for(int i=1;i<=n;i++){
+            f=f*i;
+        }
+        System.out.println("factorial of number. "+f);
+    }
+}
