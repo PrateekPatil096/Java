4529310ce6449818a086fe1aa97718a812211c85
Prateek Patil <prateekpatil096@gmail.com>
2026-07-11 14:22:48 +0530

    palnidrome check

---
+++ questions/q26.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class q26 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the string");
+        String s=sc.nextLine();
+        String rev="";
+
+        for(int i=s.length()-1;i>=0;i--){
+            rev=rev+s.charAt(i);
+        }
+
+        if(s.equals(rev)){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not palindrome");
+        }
+    }
+    
+}
