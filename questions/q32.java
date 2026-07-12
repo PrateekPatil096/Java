b3f369190d5bfeb27e8bd66f487536d6eaae3ce0
Prateek Patil <prateekpatil096@gmail.com>
2026-07-12 12:35:12 +0530

    selection sort

--- questions/q32.java
+++ questions/q32.java
@@ -14,7 +14,7 @@ public class q32 {
         }
         for(int i=0;i<n-1;i++){
             int min=i;
-            for(int j=i;j<n;j++){
+            for(int j=i+1;j<n;j++){
                 if(arr[j]<arr[min]){
                     min=j;
                 }
