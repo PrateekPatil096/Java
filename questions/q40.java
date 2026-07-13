fc4592a7d50f65547e0385f1144e2160917290ac
Prateek Patil <prateekpatil096@gmail.com>
2026-07-13 16:17:43 +0530

    sum of elements of  2D matrix

---
+++ questions/q40.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class q40 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        
+         System.out.println("enter the rows");
+        int n=sc.nextInt();
+        System.out.println("enter the col");
+        int m=sc.nextInt();
+        int sum=0;
+
+        int arr[][]=new int[n][m];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                sum=sum+arr[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+    
+}
