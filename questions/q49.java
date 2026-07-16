ccfe9992d2895ee467e4d1f71926ea5965772f29
Prateek Patil <prateekpatil096@gmail.com>
2026-07-16 16:31:40 +0530

    two sum problem by using hashing

---
+++ questions/q49.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class q49 {
+
+    public static int[] twoSumHashing(int[] nums, int target) {
+
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            
+            int complement = target - nums[i];
+
+            
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+           
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        int[] result = twoSumHashing(nums, target);
+
+        if (result[0] == -1)
+            System.out.println("No two sum solution");
+        else
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
