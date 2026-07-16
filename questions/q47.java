d487ba15856d73771e1c0b8f7158857a05e4db10
Prateek Patil <prateekpatil096@gmail.com>
2026-07-16 15:49:07 +0530

    inorder travesrasl in binary tree

---
+++ questions/q47.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class q47 {
+    public static void main(String[] args) {
+        TreeSet<Integer> tree=new TreeSet<>();
+
+        tree.add(1);
+        tree.add(2);
+        tree.add(3);
+
+        System.out.println("inorder traversal "+ tree);
+    }
+}
