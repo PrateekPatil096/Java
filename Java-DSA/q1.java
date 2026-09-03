c68810e45404299be0a0e26edf2cd98df73e0be6
Prateek Patil <prateekpatil096@gmail.com>
2026-09-03 22:35:25 +0530

    DSA question 1

---
+++ Java-DSA/q1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class q1 {
+    public static void main(String[] args) {
+       System.out.println("enter the number");
+      Scanner sc =new Scanner(System.in);
+      int n=sc.nextInt();
+      System.out.println(n);
+      
+      
+    }
+}
