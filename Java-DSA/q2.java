0f66a506afcf769d6100acaf6a23fedfe64c88c1
Prateek Patil <prateekpatil096@gmail.com>
2026-09-04 08:08:04 +0530

    cpp basics

---
+++ Java-DSA/q2.java
@@ -0,0 +1,15 @@
+public class q2 {
+    public static void main(String[] args) {
+        int age=20;
+        double price=99.090;
+        char grade='A';
+        boolean pass=true;
+
+        int sum=10+age;
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(grade);
+        System.out.println(sum);
+    }
+    
+}
