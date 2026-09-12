58c79495ba5e0c658a862ce1f8667e5ba253717f
Prateek Patil <prateekpatil096@gmail.com>
2026-09-12 15:18:56 +0530

    patterns 7 DSA

---
+++ Java-DSA/q18.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class q18 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+
+            //space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            //starts
+
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+
+            //space
+             for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
