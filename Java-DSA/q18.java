20efea80ec602d8dcc0f334acf8519ea4ae37597
Prateek Patil <prateekpatil096@gmail.com>
2026-09-12 15:45:13 +0530

    patterns 8 DSA

--- Java-DSA/q18.java
+++ Java-DSA/q18.java
@@ -1,21 +1,21 @@
 import java.util.*;
 public class q18 {
     public static void main(String[] args) {
-        int n=5;
+        int n=4;
         for(int i=0;i<n;i++){
 
             //space
-            for(int j=0;j<n-i-1;j++){
+            for(int j=0;j<i;j++){
                 System.out.print(" ");
             }
             //starts
 
-            for(int j=0;j<2*i+1;j++){
+            for(int j=0;j<2*n-(2*i +1);j++){
                 System.out.print("*");
             }
 
             //space
-             for(int j=0;j<n-i-1;j++){
+             for(int j=0;j<i;j++){
                 System.out.print(" ");
             }
             System.out.println();
