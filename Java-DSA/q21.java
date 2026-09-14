d8c08dcc6ff6753e64e85653c1138714e5aece02
Prateek Patil <prateekpatil096@gmail.com>
2026-09-14 14:05:25 +0530

    patterns 11 DSA

---
+++ Java-DSA/q21.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class q21 {
+    public static void main(String[] args) {
+        int n=4;
+
+        int start=1;
+        for(int i=0;i<n;i++){
+            if(i%2==0)
+                start=1;
+            else start=0;
+            for(int j=0;j<=i;j++){
+                System.out.print(start + " ");
+                start=1-start;
+            }
+            System.out.println();
+        }
+    }
+    
+}
