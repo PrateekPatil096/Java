c2ddd697b2a72502a55bb554548bf54babbd3936
Prateek Patil <prateekpatil096@gmail.com>
2026-09-20 11:15:38 +0530

    count digits DSA

---
+++ Java-DSA/q33.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class q33 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number ");
+        int n=sc.nextInt();
+
+        int count=0;
+
+        while(n>0){
+            n=n/10;
+            count++;
+        }
+        System.out.println("there are "+count+" digits");
+
+
+    }
+    
+}
