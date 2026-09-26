df29db586411e1f03b4610de5ebe80583e67add5
Prateek Patil <prateekpatil096@gmail.com>
2026-09-26 15:09:09 +0530

    count prime numbers till n

---
+++ Java-DSA/q42.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class q42 {
+    public static void main(String []args){
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        int count=0;
+
+        for(int i=2;i<=n;i++){
+          int factors=0;
+
+          for(int j=1;j<=i;j++){
+            if(i%j==0){
+                factors++;
+            }
+          }
+          if(factors==2){
+            count++;
+          }
+        }
+
+        System.out.println(count);
+
+
+    }
+    
+}
